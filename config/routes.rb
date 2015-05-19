8dbcaed4d1b9fb6b607d2fc812191a95d4073ef7
Xiaopow <xiaoyang88@gmail.com>
2015-05-19 15:17:06 +0800

    add deivse to app

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
+
+  # get 'users/log_out' => 'users/sessions#destroy'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
