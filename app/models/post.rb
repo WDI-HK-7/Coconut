8561dd1053268ddfe164a2bc8aa4a2f40a4fa12c
Xiaopow <xiaoyang88@gmail.com>
2015-05-20 18:54:51 +0800

    change default image to placeholdit

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ActiveRecord::Base
   belongs_to :user
   
-  has_attached_file :picture, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :picture, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "http://placehold.it/400x400"
   
   validates_attachment_content_type :picture, :content_type => /\Aimage\/.*\Z/
 end
