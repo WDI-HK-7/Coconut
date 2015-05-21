dbe5cb2f6e48e341a0a1898234235c86c74b4020
Xiaopow <xiaoyang88@gmail.com>
2015-05-21 10:01:48 +0800

    add comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,21 @@
 class CommentsController < ApplicationController
+  def create
+    @post = current_user.comments.new(comment_params)
+
+    if @post.save
+      render :json => {
+        :message => { :message => "Successful", :success => true }
+      }
+    else
+      render :json => {
+        :message => { :message => "Unsuccessful", :success => false }
+      }
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content, :post_id)
+  end
 end
