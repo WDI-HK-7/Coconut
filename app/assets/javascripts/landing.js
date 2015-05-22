d1a93b45073e0047559cefc0500d1da93936add3
Michael Chow <DexVitalitY@Michaels-MacBook-Pro.local>
2015-05-22 15:53:46 +0800

    fixed landing.js indentation

--- app/assets/javascripts/landing.js
+++ app/assets/javascripts/landing.js
@@ -1,11 +1,11 @@
 function showSignUp(target){
-	document.getElementById(target).style.display = 'block';
-	document.getElementById("signin").style.display = 'none';
+  document.getElementById(target).style.display = 'block';
+  document.getElementById("signin").style.display = 'none';
 }
 
 function hideSignUp(target){
-	document.getElementById(target).style.display = 'none';
-	document.getElementById("signin").style.display = 'block';
+  document.getElementById(target).style.display = 'none';
+  document.getElementById("signin").style.display = 'block';
 }
 
  var backgroundURL = [
