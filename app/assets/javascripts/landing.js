8a81fe2248665644e7a2d1555ab095fc91661782
Michael Chow <DexVitalitY@Michaels-MacBook-Pro.local>
2015-05-22 10:48:56 +0800

    rebasing to Dom's branch merges

---
+++ app/assets/javascripts/landing.js
@@ -0,0 +1,9 @@
+function showSignUp(target){
+	document.getElementById(target).style.display = 'block';
+	document.getElementById("signin").style.display = 'none';
+}
+
+function hideSignUp(target){
+	document.getElementById(target).style.display = 'none';
+	document.getElementById("signin").style.display = 'block';
+}
