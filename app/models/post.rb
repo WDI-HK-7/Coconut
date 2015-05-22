cb12556fcafc96e4dd1f5c5acc73158a27fea991
Xiaopow <xiaoyang88@gmail.com>
2015-05-22 10:19:23 +0800

    change all submit buttons to input, change picture large to 1000x1000> and add xlarge

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ActiveRecord::Base
   belongs_to :user
   has_many :comments
 
-  has_attached_file :picture, :convert_options => { :all => '-auto-orient' }, :styles => { :large => "2000x2000>", :medium => "300x300>", :thumb => "100x100>" }, :default_url => "http://placehold.it/400x400"
+  has_attached_file :picture, :convert_options => { :all => '-auto-orient' }, :styles => { :xlarge => "2000x2000>", :large => "1000x1000>", :medium => "300x300>", :thumb => "100x100>" }, :default_url => "http://placehold.it/400x400"
 
   validates_attachment_content_type :picture, :content_type => /\Aimage\/.*\Z/
 end
